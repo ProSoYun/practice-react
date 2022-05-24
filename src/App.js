350044a47806da6cfa272c9c636da8508b1ec664
Uni <bsy1482@naver.com>
2022-05-24 22:01:12 +0900

    2022.05.24 석이밍과 함께.. 2차

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Title from "./components/Title/Title";
+import Content from "./components/Content"
+import Fotter from "./components/Fotter";
+
+const App = () => {
+    return (
+        <>
+            <Title/>
+            <Content/>
+            <Fotter/>
+        </>
+    );
+};
 
 export default App;
